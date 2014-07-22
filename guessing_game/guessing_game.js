6a409d795eeeaf5ccf81ce4b36f7eb69fe7ba2f4
Charlie Massry <charlesdmassry@gmail.com>
2014-07-22 09:37:50 -0400

    made guessing game more robust

--- guessing_game/guessing_game.js
+++ guessing_game/guessing_game.js
@@ -1,9 +1,51 @@
-function wombat() {
-  document.getElementById("result").innerHTML = "Very Good!!";
-    return false;
+ var images = [
+         { kind: "wombat", src: "https://farm8.staticflickr.com/7163/6642873613_89c447df26_o.jpg" },
+        { kind: "wombat", src: "http://4.bp.blogspot.com/_X1IWXuEbgXI/S7r6QmBIqPI/AAAAAAAACbo/gkCdqFOxbbM/s640/wombat+australia.jpg" },
+        { kind: "capybara", src: "http://www.zooborns.com/.a/6a010535647bf3970b019b02308fe1970b-800wi" },
+        { kind: "capybara", src: "http://a-z-animals.com/media/animals/images/original/capybara7.jpg" },
+        { kind: "capybara", src: "http://www.zooborns.com/.a/6a010535647bf3970b019b02308ed6970b-800wi" },
+        { kind: "wombat", src: "http://resources0.news.com.au/images/2011/07/07/1226089/905572-wombat.jpg" }
+      ];
+
+var currentImage = 0;
+var correctCount = 0;
+
+function next() {
+  currentImage++;
+  setImage();
 }
 
-function capybara(){
-document.getElementById("result").innerHTML = "You're wrong!!";
-return false;
+function setImage() {
+  $("#current").attr("src", images[currentImage].src);
 }
+
+function isCorrect(answer) {
+  return $("a.button#" + answer).attr("id") === images[currentImage].kind;
+}
+
+function handleCorrect() {
+  return correctCount++;
+}
+
+function handleIncorrect() {
+  return false;
+}
+
+function last() {
+  return $("#result").html("You have guessed " + correctCount + " / " + images.length);
+}
+
+$(function() {
+  setImage();
+
+  $("a.button").click(function() {
+    if(isCorrect(this.id)) {
+      handleCorrect();
+    }
+    if(currentImage >= 5) {
+      last();
+    } else {
+      next();
+    }
+  });
+});
